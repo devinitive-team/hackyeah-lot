c75409666a1a2d6bd41fc7794719a48506c3fb00
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-09-14 18:32:34 +0200

    Setup mongo (#3)
    * Install config and mongoose
    
    * Add default config
    
    * Use mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 
+import { DatabaseModule} from "./database/database.module";
+
 import { CitiesResolver } from "./cities/cities.resolver";
 
 @Module({
   imports: [
+    DatabaseModule,
     GraphQLModule.forRoot({
       debug: false,
       playground: true,
