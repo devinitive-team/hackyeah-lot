a0801297214113c4471bdce8c0b1e27341daa32a
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-09-14 20:57:51 +0200

    Finish city model (#9)
    * Finish City Model for gQl
    
    * Fix typo
    
    * Add new fields to mongo model
    
    * Add new fields to city interface

--- src/city/city.model.ts
+++ src/city/city.model.ts
@@ -1,8 +1,11 @@
 import { Schema } from "mongoose";
 
 export const CityModel = new Schema({
+  advertisement: String,
   category: String,
   code: String,
   description: String,
   name: String,
+  imagesSmall: [String],
+  imagesBig: [String],
 });
