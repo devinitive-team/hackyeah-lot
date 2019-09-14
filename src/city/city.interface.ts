b5ad268f40a1f8a5757640db691cce5edd272c8e
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-09-14 20:54:08 +0200

    Add new fields to city interface

--- src/city/city.interface.ts
+++ src/city/city.interface.ts
@@ -1,8 +1,12 @@
 import { Document } from "mongoose";
 
 export interface ICity extends Document {
+  id: string;
+  advertisement: string;
   category: string;
   code: string;
   description: string;
   name: string;
+  imagesSmall: string[];
+  imagesBig: string[];
 }
